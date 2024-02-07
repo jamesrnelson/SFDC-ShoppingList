6e67756050a0c8b48cc042aee21aefbcafc41333
Jimmy Nelson <nelson.jimmy@gmail.com>
2024-02-06 19:32:50 -0700

    Creates new addIngredient component that will pass info to the reusable ingredient search component.

---
+++ force-app/main/default/lwc/addIngredient/addIngredient.js
@@ -0,0 +1,30 @@
+import { LightningElement, track, api, wire } from 'lwc';
+
+export default class AddIngredient extends LightningElement {
+    @api recordId;
+    @api label;
+    @api createOrEdit;
+    @api objectApiName;
+    @api recordType;  //expects the record type label (NOT the record type developer name)
+    @api relatedParentRecord;
+    @api parentLookupField;
+    @api variant;
+    @api insertedObject;
+
+    @track showObjectModal;
+    @track objectInfo;
+
+    
+    handleCancel(event) {
+        this.closeObjectModal();
+    }
+
+    openObjectModal() {
+        this.showObjectModal = true;
+    }
+
+    closeObjectModal() {
+        this.showObjectModal = false;
+        //this.resetModalSpinner();
+    }
+}
