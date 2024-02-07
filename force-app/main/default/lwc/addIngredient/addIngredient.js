bfe22568cd2ceabc8691c5d8447472d423944c5c
Jimmy Nelson <nelson.jimmy@gmail.com>
2024-02-07 14:50:32 -0700

    dispatches event on modal close to reload shopping list ingredientts

--- force-app/main/default/lwc/addIngredient/addIngredient.js
+++ force-app/main/default/lwc/addIngredient/addIngredient.js
@@ -24,6 +24,7 @@ export default class AddIngredient extends LightningElement {
     }
 
     closeObjectModal() {
+        this.dispatchEvent(new CustomEvent('reloadshoppinglistingredients'));
         this.showObjectModal = false;
         //this.resetModalSpinner();
     }
